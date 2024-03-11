069f62dc226cfdcdce2802aaeda1203ed45954ec
hardikgupta2003 <hardikgupta5172@gmail.com>
2024-03-11 23:29:59 +0530

    Added Dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
-import { Route,Routes } from "react-router-dom";
+import { Route,Routes, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from './components/common/Navbar'
 import OpenRoute from "./components/core/Auth/OpenRoute";
+import PrivateRoute from "./components/core/Auth/PrivateRoute";
+
 
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
@@ -10,9 +12,23 @@ import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import About from "./pages/About";
+import MyProfile from '../src/components/core/Dashboard/MyProfile'
+import Dashboard from './pages/Dashboard'
+import Settings from "./components/core/Dashboard/Settings";
+import Cart from './components/core/Dashboard/Cart'
+import { useDispatch, useSelector } from "react-redux";
+import {ACCOUNT_TYPE} from "./utils/constants"
+import EnrolledCourses from './components/core/Dashboard/EnrolledCourses'
+import Error from "./pages/Error"
+
+
 
 
 function App() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  const { user } = useSelector((state) => state.profile)
   return (
       <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
         <Navbar/>
@@ -70,6 +86,31 @@ function App() {
             </OpenRoute>
           }
         />  
+ <Route 
+      element={
+        <PrivateRoute>
+          <Dashboard />
+        </PrivateRoute>
+      }
+    >
+      <Route path="dashboard/my-profile" element={<MyProfile />} />
+      <Route path="dashboard/Settings" element={<Settings />} />
+      
+
+      {
+        user?.accountType === ACCOUNT_TYPE.STUDENT && (
+          <>
+          <Route path="dashboard/cart" element={<Cart />} />
+          <Route path="dashboard/enrolled-courses" element={<EnrolledCourses />} />
+          </>
+        )
+      }
+
+
+    </Route>
+      
+    <Route path="*" element={<Error />} />
+
         </Routes>
       </div>
   );
